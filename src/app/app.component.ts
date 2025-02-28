8e4eb421d38a1da9fc52c3bc5a81ad1b1d83fc03
Aleksander <sokhoron@gmail.com>
2025-02-28 21:22:12 +0300

    add content in main

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from "./components/footer/footer.component";
 import { HeaderComponent } from "./components/header/header.component";
+import {MatIconModule, MatIconRegistry} from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FooterComponent, HeaderComponent],
+  imports: [RouterOutlet, FooterComponent, HeaderComponent, MatIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'EliteTechnologies';
+  constructor(private iconRegistry: MatIconRegistry) {}
+
+  ngOnInit(): void {
+    this.iconRegistry.addSvgIconLiteral('backIcon', 'assets/icons/back.svg');
+    this.iconRegistry.addSvgIconLiteral('nextIcon', 'assets/icons/next.svg');
+  }
 }
