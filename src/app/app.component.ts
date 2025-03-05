8dcc416a305dd424cce45fb5f58ca42f9cddbdc6
Andrei Muha <andrei.muha@rubyroidlabs.com>
2025-03-05 19:08:33 +0300

    add services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from "./components/footer/footer.component";
 import { HeaderComponent } from "./components/header/header.component";
-import {MatIconModule, MatIconRegistry} from '@angular/material/icon';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FooterComponent, HeaderComponent, MatIconModule],
+  imports: [RouterOutlet, FooterComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnInit{
+export class AppComponent{
   title = 'EliteTechnologies';
-  constructor(private iconRegistry: MatIconRegistry) {}
-
-  ngOnInit(): void {
-    this.iconRegistry.addSvgIconLiteral('backIcon', 'assets/icons/back.svg');
-    this.iconRegistry.addSvgIconLiteral('nextIcon', 'assets/icons/next.svg');
-  }
 }
--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
-
+import { Component, inject } from '@angular/core';
+import {MatIconRegistry, MatIconModule} from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 @Component({
   selector: 'app-main',
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './main.component.html',
-  styleUrl: './main.component.scss'
+  styleUrl: './main.component.scss',
+  standalone: true,
+
 })
 export class MainComponent {
+  constructor() {
+    const iconRegistry = inject(MatIconRegistry);
+    const sanitizer = inject(DomSanitizer);
+    iconRegistry.addSvgIcon(
+      'arrow',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/icons/arrow.svg')
+    );
+  }
 
 }
